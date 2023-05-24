be94eb068e6fecd733d305f56396708cffb95c4e
Caroline Frasca <103123936+sfc-gh-cfrasca@users.noreply.github.com>
2023-05-24 17:33:13 -0400

    Add text box for API key

--- pages/File_Q&A.py
+++ pages/File_Q&A.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import anthropic
 
+with st.sidebar:
+    anthropic_api_key = st.text_input('Anthropic API Key')
+
 st.title("📝 File Q&A with Anthropic")
 uploaded_file = st.file_uploader("Upload an article", type="txt")
 question = st.text_input(
@@ -14,7 +17,8 @@ if uploaded_file and question:
     prompt = f"""{anthropic.HUMAN_PROMPT} Here's an article:\n\n<article>
     {article}\n\n</article>\n\n{question}{anthropic.AI_PROMPT}"""
 
-    client = anthropic.Client(st.secrets.anthropic_api_key)
+    client = anthropic.Client(anthropic_api_key)
+    #client = anthropic.Client(st.secrets.anthropic_api_key)
     response = client.completion(
         prompt=prompt,
         stop_sequences=[anthropic.HUMAN_PROMPT],
