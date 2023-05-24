1bb727a2c75c726a0a02c2483c3053485020bb09
Caroline Frasca <103123936+sfc-gh-cfrasca@users.noreply.github.com>
2023-05-24 17:40:36 -0400

    Update LangChain_Search.py

--- pages/LangChain_Search.py
+++ pages/LangChain_Search.py
@@ -11,20 +11,27 @@ with st.sidebar:
 st.title("🔎 Search with LangChain")
 question = st.text_input("What do you want to know?", placeholder="Who won the Women's U.S. Open in 2018?")
 
-#llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=open_ai_api_key)
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=st.secrets.openai_api_key)
-
-# search = GoogleSerperAPIWrapper(serper_api_key=st.secrets.serper_api_key)
-search = GoogleSerperAPIWrapper(serper_api_key=serper_api_key)
-
-search_tool = Tool(
-    name="Intermediate Answer",
-    func=search.run,
-    description="useful for when you need to ask with search",
-)
-search_agent = initialize_agent([search_tool], llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
-
 if question:
-    response = search_agent.run(question)
-    st.write("### Answer")
-    st.write(response)
+    if not serper_api_key and not open_ai_api_key:
+        st.info("Please add your Serper and OpenAI API keys to continue.")
+    elif not serper_api_key:
+        st.info("Please add your Serper API key to continue.")
+    elif not open_ai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+    else:
+        #llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=open_ai_api_key)
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=st.secrets.openai_api_key)
+
+        # search = GoogleSerperAPIWrapper(serper_api_key=st.secrets.serper_api_key)
+        search = GoogleSerperAPIWrapper(serper_api_key=serper_api_key)
+
+        search_tool = Tool(
+            name="Intermediate Answer",
+            func=search.run,
+            description="useful for when you need to ask with search",
+        )
+        search_agent = initialize_agent([search_tool], llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+
+        response = search_agent.run(question)
+        st.write("### Answer")
+        st.write(response)
