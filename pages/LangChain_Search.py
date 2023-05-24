adedc96a954065d19924db7931cca97634e49c7e
Caroline Frasca <103123936+sfc-gh-cfrasca@users.noreply.github.com>
2023-05-24 17:46:08 -0400

    Update LangChain_Search.py

--- pages/LangChain_Search.py
+++ pages/LangChain_Search.py
@@ -6,20 +6,20 @@ from langchain.agents import AgentType
 
 with st.sidebar:
     serper_api_key = st.text_input('Serper API Key')
-    open_ai_api_key = st.text_input('OpenAI API Key')
+    openai_api_key = st.text_input('OpenAI API Key')
 
 st.title("🔎 Search with LangChain")
 question = st.text_input("What do you want to know?", placeholder="Who won the Women's U.S. Open in 2018?")
 
 if question:
-    if not serper_api_key and not open_ai_api_key:
+    if not serper_api_key and not openai_api_key:
         st.info("Please add your Serper and OpenAI API keys to continue.")
     elif not serper_api_key:
         st.info("Please add your Serper API key to continue.")
-    elif not open_ai_api_key:
+    elif not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
-    else:
-        llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=open_ai_api_key)
+    elif serper_api_key and openai_api_key:
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key)
 #         llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=st.secrets.openai_api_key)
 
         search = GoogleSerperAPIWrapper(serper_api_key=serper_api_key)
