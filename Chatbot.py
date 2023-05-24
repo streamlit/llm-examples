04c3918f8d3bc7f5ef49bb1a32123c53d01b2704
Caroline Frasca <103123936+sfc-gh-cfrasca@users.noreply.github.com>
2023-05-24 17:20:19 -0400

    Update and rename streamlit_app.py to Chatbot.py

---
+++ Chatbot.py
@@ -0,0 +1,28 @@
+import openai
+import streamlit as st
+from streamlit_chat import message
+
+st.title("💬 Streamlit GPT")
+openai.api_key = st.secrets.openai_api_key
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+with st.form("chat_input", clear_on_submit=True):
+    a, b = st.columns([4, 1])
+    user_input = a.text_input(
+        label="Your message:",
+        placeholder="What would you like to say?",
+        label_visibility="collapsed",
+    )
+    b.form_submit_button("Send", use_container_width=True)
+
+for msg in st.session_state.messages:
+    message(msg["content"], is_user=msg["role"] == "user")
+
+if user_input:
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    message(user_input, is_user=True)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    message(msg.content)
