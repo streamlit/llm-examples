d590a413e70be7a7d42367854ad34eda18893ffb
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-26 12:50:50 +0700

    Update 3_Langchain_PromptTemplate.py

--- pages/3_Langchain_PromptTemplate.py
+++ pages/3_Langchain_PromptTemplate.py
@@ -4,11 +4,12 @@ from langchain import PromptTemplate
 
 st.title('🦜🔗 Langchain - Blog Outline Generator App')
 
-with st.sidebar:
-  openai_api_key = st.text_input('OpenAI API Key')
+#with st.sidebar:
+#  openai_api_key = st.text_input('OpenAI API Key')
   
 def blog_outline(topic):
   # Instantiate LLM model
+  openai_api_key = st.sidebar.text_input('OpenAI API Key')
   llm = OpenAI(model_name='text-davinci-003', openai_api_key=openai_api_key)
   # Prompt
   template = 'As an experienced data scientist and technical writer, generate an outline for a blog about {topic}.'
