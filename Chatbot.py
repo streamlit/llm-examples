47972053b531639783fe9ee3562699729a2e8bee
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-26 11:07:37 +0700

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -8,7 +8,7 @@ with st.sidebar:
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 st.title("💬 Streamlit GPT")
-#openai.api_key = st.secrets.openai_api_key
+openai.api_key = st.secrets.openai_api_key
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
