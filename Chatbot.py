a069ff51b6bb8f161ee1a7b9db339655cef6954c
Caroline Frasca <103123936+sfc-gh-cfrasca@users.noreply.github.com>
2023-06-02 14:46:52 -0400

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -3,7 +3,7 @@ import streamlit as st
 from streamlit_chat import message
 
 with st.sidebar:
-    openai_api_key = st.text_input(key='chatbot_api_key','OpenAI API Key')
+    openai_api_key = st.text_input('OpenAI API Key',key='chatbot_api_key')
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
