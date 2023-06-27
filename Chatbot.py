1b2d90554c1538cd3c1a2aa6af20811845d77878
Zachary Blackwood <zachary@streamlit.io>
2023-06-26 18:00:28 -0700

    Make some small fixes

--- Chatbot.py
+++ Chatbot.py
@@ -3,14 +3,17 @@ import streamlit as st
 from streamlit_chat import message
 
 with st.sidebar:
-    openai_api_key = st.text_input('OpenAI API Key',key='chatbot_api_key')
+    openai_api_key = st.text_input(
+        "OpenAI API Key", key="chatbot_api_key", type="password"
+    )
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 st.title("💬 Streamlit GPT")
-#openai.api_key = st.secrets.openai_api_key
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [
+        {"role": "assistant", "content": "How can I help you?"}
+    ]
 
 with st.form("chat_input", clear_on_submit=True):
     a, b = st.columns([4, 1])
@@ -21,17 +24,19 @@ with st.form("chat_input", clear_on_submit=True):
     )
     b.form_submit_button("Send", use_container_width=True)
 
-for msg in st.session_state.messages:
-    message(msg["content"], is_user=msg["role"] == "user")
+for idx, msg in enumerate(st.session_state.messages):
+    message(msg["content"], is_user=msg["role"] == "user", key=idx)
 
 if user_input and not openai_api_key:
     st.info("Please add your OpenAI API key to continue.")
-    
+
 if user_input and openai_api_key:
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": user_input})
     message(user_input, is_user=True)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=st.session_state.messages
+    )
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
     message(msg.content)
--- pages/LangChain_Search.py
+++ pages/LangChain_Search.py
@@ -5,13 +5,19 @@ from langchain.agents import initialize_agent, Tool
 from langchain.agents import AgentType
 
 with st.sidebar:
-    serper_api_key = st.text_input('Serper API Key',key='langchain_search_api_key_serper')
-    openai_api_key = st.text_input('OpenAI API Key',key='langchain_search_api_key_openai')
+    serper_api_key = st.text_input(
+        "Serper API Key", key="langchain_search_api_key_serper", type="password"
+    )
+    openai_api_key = st.text_input(
+        "OpenAI API Key", key="langchain_search_api_key_openai", type="password"
+    )
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/LangChain_Search.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 st.title("🔎 Search with LangChain")
-question = st.text_input("What do you want to know?", placeholder="Who won the Women's U.S. Open in 2018?")
+question = st.text_input(
+    "What do you want to know?", placeholder="Who won the Women's U.S. Open in 2018?"
+)
 
 if question:
     if not serper_api_key and not openai_api_key:
@@ -22,17 +28,20 @@ if question:
         st.info("Please add your OpenAI API key to continue.")
     elif serper_api_key and openai_api_key:
         llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key)
-#         llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=st.secrets.openai_api_key)
 
         search = GoogleSerperAPIWrapper(serper_api_key=serper_api_key)
-#         search = GoogleSerperAPIWrapper(serper_api_key=st.secrets.serper_api_key)
 
         search_tool = Tool(
             name="Intermediate Answer",
             func=search.run,
             description="useful for when you need to ask with search",
         )
-        search_agent = initialize_agent([search_tool], llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+        search_agent = initialize_agent(
+            [search_tool],
+            llm,
+            agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+            verbose=True,
+        )
 
         response = search_agent.run(question)
         st.write("### Answer")
