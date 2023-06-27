cbb4b093a209bc64fc71aabbf47f19c0623c505c
Alex Atallah <alexander.atallah@gmail.com>
2023-06-26 20:29:58 -0400

    add pkce oauth

--- components/Sidebar.py
+++ components/Sidebar.py
@@ -4,16 +4,18 @@ import requests
 import json
 from shared import constants
 
+
 # Get available models from the API
 def get_available_models():
     try:
-        response = requests.get(constants.OPENROUTER_API_BASE + '/models')
+        response = requests.get(constants.OPENROUTER_API_BASE + "/models")
         response.raise_for_status()
         models = json.loads(response.text)["data"]
-        return [model['id'] for model in models]
+        return [model["id"] for model in models]
     except requests.exceptions.RequestException as e:
         st.error(f"Error getting models from API: {e}")
         return []
+
 
 # Handle the model selection process
 def handle_model_selection(available_models, selected_model, default_model):
@@ -22,23 +24,49 @@ def handle_model_selection(available_models, selected_model, default_model):
         selected_index = available_models.index(selected_model)
     else:
         selected_index = available_models.index(default_model)
-    selected_model = st.selectbox('Select a model', available_models, index=selected_index)
+    selected_model = st.selectbox(
+        "Select a model", available_models, index=selected_index
+    )
     return selected_model
+
+
+def exchange_code_for_api_key(code: str):
+    print(f"Exchanging code for API key: {code}")
+    try:
+        response = requests.post(
+            constants.OPENROUTER_API_BASE + "/auth/keys",
+            json={"code": code},
+        )
+        response.raise_for_status()
+        st.experimental_set_query_params()
+        api_key = json.loads(response.text)["key"]
+        st.session_state["api_key"] = api_key
+        st.experimental_rerun()
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error exchanging code for API key: {e}")
+
 
 def sidebar(default_model):
     with st.sidebar:
         params = st.experimental_get_query_params()
+        code = params.get("code", [""])[0]
+        if code:
+            exchange_code_for_api_key(code)
         # not storing sensitive api_key in query params
         api_key = st.session_state.get("api_key")
-        selected_model = params.get("model", [None])[0] or st.session_state.get("model", None)
+        selected_model = params.get("model", [None])[0] or st.session_state.get(
+            "model", None
+        )
         if not api_key:
             if st.button("Connect OpenRouter"):
                 webbrowser.open(
-                    "https://openrouter.ai/account?callback_url=http://localhost:8501",
+                    f"{constants.OPENROUTER_BASE}/auth?callback_url=http://localhost:8501",
                     new=0,
                 )
         available_models = get_available_models()
-        selected_model = handle_model_selection(available_models, selected_model, default_model)
+        selected_model = handle_model_selection(
+            available_models, selected_model, default_model
+        )
         st.session_state["model"] = selected_model
         st.experimental_set_query_params(model=selected_model)
 
@@ -47,7 +75,11 @@ def sidebar(default_model):
             if st.button("Log out"):
                 del st.session_state["api_key"]
                 st.experimental_rerun()
-        st.markdown("[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)")
-        st.markdown("[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)")
-        
+        st.markdown(
+            "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+        )
+        st.markdown(
+            "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+        )
+
     return api_key, selected_model
--- shared/constants.py
+++ shared/constants.py
@@ -1,5 +1,7 @@
 OPENROUTER_REFERER = "https://openrouter.streamlit.io"
-OPENROUTER_API_BASE = "https://openrouter.ai/api/v1"
+OPENROUTER_BASE = "http://localhost:3000"
+# OPENROUTER_BASE = "https://openrouter.ai"
+OPENROUTER_API_BASE = f"{OPENROUTER_BASE}/api/v1"
 
 OPENROUTER_DEFAULT_CHAT_MODEL = "openai/gpt-3.5-turbo-16k"
 OPENROUTER_DEFAULT_INSTRUCT_MODEL = "anthropic/claude-v1-100k"
