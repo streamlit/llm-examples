bd299a0bdcf2bd5bb48b42f7017ee722c09bfa4f
Joshua Carroll <joshua.carroll@snowflake.com>
2023-06-28 13:41:09 -0700

    Change title on chatbot

--- Chatbot.py
+++ Chatbot.py
@@ -6,7 +6,7 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("💬 Streamlit GPT")
+st.title("💬 Chatbot")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
