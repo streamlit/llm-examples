b0420c31b861f0e415a86abea53dba1944709747
Daniel Olshansky <olshansky.daniel@gmail.com>
2023-07-18 18:15:27 -0700

    Add a link to where you can get an OpenAI API Key

--- pages/2_Chat_with_search.py
+++ pages/2_Chat_with_search.py
@@ -7,6 +7,7 @@ from langchain.tools import DuckDuckGoSearchRun
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/2_Chat_with_search.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
