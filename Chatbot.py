ba51b9b6ee478444f6c5534db9eeff20af71d26d
Maggie Liu <121910051+sfc-gh-maliu@users.noreply.github.com>
2023-08-10 14:25:12 -0700

    add some description

--- Chatbot.py
+++ Chatbot.py
@@ -8,6 +8,7 @@ with st.sidebar:
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 st.title("💬 Chatbot")
+st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
