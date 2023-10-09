f1d24690e8357aa6b07e2cc672a1b9e2dd387957
Joshua Carroll <joshua.carroll@snowflake.com>
2023-10-09 10:50:22 -0700

    Add Langchain Quickstart test

--- app_test.py
+++ app_test.py
@@ -16,7 +16,7 @@ def create_openai_object_sync(response: str, role: str = "assistant") -> OpenAIO
 
 
 @patch("openai.ChatCompletion.create")
-def test_basic_chat(openai_create):
+def test_Chatbot(openai_create):
     at = AppTest.from_file("Chatbot.py")
     # Todo: Remove the key in app and fix this once we have chat_input support
     at.session_state["chat"] = "Do you know any jokes?"
@@ -31,3 +31,16 @@ def test_basic_chat(openai_create):
     assert at.get("chat_message")[1].markdown[0].value == "Do you know any jokes?"
     assert at.get("chat_message")[2].markdown[0].value == JOKE
     assert not at.exception
+
+
+@patch("langchain.llms.OpenAI.__call__")
+def test_Langchain_Quickstart(langchain_llm):
+    at = AppTest.from_file("pages/3_Langchain_Quickstart.py").run()
+    assert at.get("alert")[0].proto.body == "Please add your OpenAI API key to continue."
+
+    RESPONSE = "1. The best way to learn how to code is by practicing..."
+    langchain_llm.return_value = RESPONSE
+    at.sidebar.text_input[0].set_value("sk-...")
+    at.button[0].set_value(True).run()
+    print(at)
+    assert at.get("alert")[0].proto.body == RESPONSE
