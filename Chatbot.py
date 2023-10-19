8880f000ebf0d0ac13f31fb9b5df28cbf8757369
mint-jiraphinya wechabut <144240103+Bimint@users.noreply.github.com>
2023-10-19 20:09:44 +0700

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -10,7 +10,7 @@ with st.sidebar:
 st.title("💬 Chatbot")
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "วันนี้ให้ มิ้น ช่วยอะไร?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
