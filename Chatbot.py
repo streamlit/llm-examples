43fb81578ca423eef9bed07cf19fb3c69425b0fa
Ahmad Nawaz <120183338+ahmad7890788@users.noreply.github.com>
2023-12-13 10:03:07 +0500

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,29 +1,65 @@
-from openai import OpenAI
 import streamlit as st
+import openai
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+# Set your OpenAI API key
+openai.api_key = "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"
 
-st.title("💬 Chatbot")
-st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+# Initialize session state
+if 'user_input' not in st.session_state:
+    st.session_state['user_input'] = []
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+if 'openai_response' not in st.session_state:
+    st.session_state['openai_response'] = []
 
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
+def api_calling(prompt):
+    completions = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    message_text = completions.choices[0].text
+    return message_text
 
-    client = OpenAI(api_key=openai_api_key)
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message.content
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
+def message(text, key, avatar_style="icons", is_user=False):
+    # Display messages in the chat interface
+    if not is_user:
+        st.text(text)
+    else:
+        st.text("User: " + text)
+
+st.title("ChatGPT ChatBot With Streamlit and OpenAI")
+
+def get_text():
+    input_text = st.text_input("Write here", key="input")
+    return input_text
+
+user_input = get_text()
+
+if user_input:
+    try:
+        output = api_calling(user_input)
+        output = output.lstrip("\n")
+
+        # Store the output
+        st.session_state.openai_response.append(output)
+        st.session_state.user_input.append(user_input)
+
+    except Exception as e:
+        st.error(f"Error: {str(e)}")
+
+# Display the chat history
+user_input_history = st.session_state.get('user_input', [])
+for i, user_input in enumerate(user_input_history):
+    message(user_input, key=str(i), avatar_style="icons", is_user=True)
+    message(st.session_state['openai_response'][i], key=str(i) + 'data_by_user', avatar_style="miniavs")
+
+# Display the current user input
+if user_input:
+    message(user_input, key="current_user_input", avatar_style="icons", is_user=True)
+
+# Display the current AI response
+if st.session_state.get('openai_response'):
+    message(st.session_state['openai_response'][-1], key="current_ai_response", avatar_style="miniavs")
