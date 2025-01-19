99faa037354d856067192f3876c8e780d9a2938e
Lucas <lrsmello.lucas@gmail.com>
2025-01-19 04:13:31 -0300

    deploy-gcp

--- happy-kids/lulu_teaches/settings.py
+++ happy-kids/lulu_teaches/settings.py
@@ -22,13 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--orlgyea(#!x$gds+#37bpz6&c$u4z6*#i+*$#zvt)87q$l2vp'
+#SECRET_KEY = 'django-insecure--orlgyea(#!x$gds+#37bpz6&c$u4z6*#i+*$#zvt)87q$l2vp'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "lulu-teaches-988538575854.europe-west3.run.app",
+    "localhost", 
+    "127.0.0.1",
+    "https://lulu-teaches-988538575854.europe-west3.run.app"
+]
 
 # Application definition
 
@@ -92,11 +97,15 @@ WSGI_APPLICATION = 'lulu_teaches.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.{}'.format(os.getenv('DATABASE_ENGINE', 'sqlite3')),
+         'NAME': os.getenv('DATABASE_NAME', 'polls'),
+         'USER': os.getenv('DATABASE_USERNAME', 'myprojectuser'),
+         'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
+         'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+         'PORT': os.getenv('DATABASE_PORT', 5432),
+     }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
